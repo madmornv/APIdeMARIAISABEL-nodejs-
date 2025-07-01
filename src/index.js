f026cff61235fecbf8c89b30cb39022c6849e324
Maria Isabel Zapata Cano <isacanooo1299@gmail.com>
2025-07-01 09:38:39 -0500

    correcion del puerto

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import app from './app.js';
 import { PORT } from './config.js';
 
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
