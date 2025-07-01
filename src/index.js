bb2ba3b8486d617151f55f6de47bfdb685ce3933
Maria Isabel Zapata Cano <isacanooo1299@gmail.com>
2025-07-01 09:51:31 -0500

    HOLA

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import app from './app.js';
 import { PORT } from './config.js';
 
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
